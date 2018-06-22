b0bc8d608709874341b6193ca5a758306304b298
kharhys <nrhyska@gmail.com>
2018-06-22 16:14:42 +0300

    added prompts for sequelize options

--- sao.js
+++ sao.js
@@ -29,6 +29,85 @@ module.exports = {
       message: 'Queue background jobs with redis?',
       default: true
     },
+
+    redis_host: {
+      message: 'Redis server host address',
+      when: function({cache, resque}) {
+        return !!(cache || resque)
+      },
+      default: '127.0.0.1'
+    },
+    redis_port: {
+      message: 'Redis server host port',
+      when: function({cache, resque}) {
+        return !!(cache || resque)
+      },
+      default: '6379'
+    },
+    redis_database: {
+      message: 'Redis database to use',
+      when: function({cache, resque}) {
+        return !!(cache || resque)
+      },
+      default: '0'
+    },
+    redis_password: {
+      message: 'Password for Redis database',
+      when: function({cache, resque}) {
+        return !!(cache || resque)
+      },
+      default: ''
+    },
+
+
+    sequelize_dialect: {
+      message: 'What dialect of SQL database will you be using',
+      when: function(database) {
+        database == 'sql'
+      },
+      type: 'list',
+      choices: [
+        { name: 'SQLite', value: 'sqlite', short: 'sqlite' },
+        { name: 'MySQL', value: 'mysql', short: 'mysql' },
+        { name: 'MSSQL', value: 'mssql', short: 'mssql' },
+        { name: 'MSSQL', value: 'postgres', short: 'postgres' }
+      ],
+      default: 'memory'
+    },
+    sequelize_host: {
+      message: 'SQL Database host',
+      when: function(database) {
+        return database == 'sql'
+      },
+      default: '127.0.0.1'
+    },
+    sequelize_port: {
+      message: 'SQL Database port',
+      when: function(database) {
+        return database == 'sql'
+      },
+      default: '3306'
+    },
+    sequelize_database: {
+      message: 'SQL Database name',
+      when: function(database) {
+        return database == 'sql'
+      }
+    },
+    sequelize_username: {
+      message: 'SQL Database username',
+      when: function(database) {
+        return database == 'sql'
+      }
+    },
+    sequelize_password: {
+      message: 'SQL Database password',
+      when: function(database) {
+        return database == 'sql'
+      }
+    },
+
+
     username: {
       message: 'What is your GitHub username?',
       default: ':gitUser:',
@@ -46,6 +125,7 @@ module.exports = {
       },
       store: true
     }
+    
   },
   move: {
     'gitignore': '.gitignore'
